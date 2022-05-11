53815a98937bb093b8d70a290d19b4b8ab1784be
preyasgarg <preyasgarg01@gmail.com>
2022-05-11 21:44:39 +0530

    .

---
+++ Mess-management-frontent/src/emojisliders.js
@@ -0,0 +1,19 @@
+// import React, { Component } from 'react'
+ 
+// import EmojiRating from 'react-emoji-rating'
+ 
+
+ 
+// export default class App extends Component {
+//     handleRating = (rating) => {
+//       console.log('RATING -> ' + rating)
+//     }
+//     return (
+//       <div>
+//         <EmojiRating 
+//           variant='classic'
+//           onChange={this.handleRating} />
+//       </div>
+//     )
+//   }
+// export default Emojisliders;
